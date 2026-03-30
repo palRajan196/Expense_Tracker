d25714c92f991892ecf8d284a51130c8bfe96e7d
palRajan196 <palrajan196@gmail.com>
2026-03-31 00:00:13 +0530

    ready for deploy

--- Node/Express.js
+++ Node/Express.js
@@ -11,6 +11,7 @@ app.use(cors());
 app.use(express.json());
 env.config();
 const MongoURL = process.env.Atlas_URL;
+const PORT = process.env.PORT || 5000;
 
 //mongoose.connect("mongodb://localhost:27017/roommate_expense");
 mongoose.connect(MongoURL);
@@ -106,4 +107,4 @@ app.get("/balance", async (req, res) => {
   res.json({ total, share, result });
 });
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
