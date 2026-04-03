8a9a96e165561cf3230d6765218799260f84dcc1
palRajan196 <palrajan196@gmail.com>
2026-04-04 01:18:25 +0530

    Change color of input

--- React/src/Greeting.jsx
+++ React/src/Greeting.jsx
@@ -5,8 +5,8 @@ function Greeting(){
     function greet(){
     const hours = new Date().getHours();
     if(hours < 12 && hours >= 3) return "Good Morning";
-    if(hours < 17) return "Good Afternoon";
-    if(hours < 21) return "Good Evening";
+    if(hours < 17 && hours >= 12) return "Good Afternoon";
+    if(hours < 21 && hours >= 17) return "Good Evening";
     return "Good Night";
     }
 
